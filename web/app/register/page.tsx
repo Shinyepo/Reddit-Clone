aad0653d89572af667768529026cec9af2ae796a
Shinyepo <shinyepo42@gmail.com>
2023-10-13 14:30:25 +0200

    prevent logged in from registering

--- web/app/register/page.tsx
+++ web/app/register/page.tsx
@@ -1,6 +1,13 @@
+import { useSession } from "next-auth/react";
 import { RegisterForm } from "./form";
+import { useRouter } from "next/navigation";
 
 export default function RegisterPage() {
+  const session = useSession();
+  const router = useRouter();
+
+  if (session.status === "authenticated") return router.push("/");
+
   return (
     <div
       style={{
